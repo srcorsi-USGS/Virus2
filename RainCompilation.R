eee0c1ccecede21e4ba8998bc62fa3b83fefb84a
SR Corsi <srcorsi@gmail.com>
2012-03-06 01:06:16 -0600

    Computed loads for human viruses, graphs of loads conditioned by site and event, refined compilation scripts

--- RainCompilation.R
+++ RainCompilation.R
@@ -16,6 +16,7 @@ excel <- "%m/%d/%Y %H:%M"
 source(paste(Rlocal,"/Rainmaker/RRainmaker.R",sep=""))
 source(paste(Rlocal,"/Rainmaker/fxn_RMeventsSamples.R",sep=""))
 source(paste(Rlocal,"/Rainmaker/fxn_RMevents.plotQ.R",sep=""))
+source(paste(Rlocal,"/Hydrovol/Fxn_WQcompos.R",sep=""))
 
 setwd(paste(Rlocal,"/MMSD_virus/Virus2/Rain",sep=""))
 #setwd(paste(Project,sep=""))
@@ -55,10 +56,10 @@ for (i in 1:length(rain.site)){
   dfsamples2$start_date <- as.character(dfsamples2$start_date)
   dfsamples2$end_date <- as.character(dfsamples2$end_date)
   
-  for (i in 1:nrow(dfsamples2)){
-    if(dfsamples2$Sample.Type.x[i]=="Baseflow"){
-      dfsamples2$start_date[i] <- as.character(dfsamples2[i,"SSdate"])
-      dfsamples2$end_date[i] <- as.character(dfsamples2[i,"SEdate"])
+  for (j in 1:nrow(dfsamples2)){
+    if(dfsamples2$Sample.Type.x[j]=="Baseflow"){
+      dfsamples2$start_date[j] <- as.character(dfsamples2[j,"SSdate"])
+      dfsamples2$end_date[j] <- as.character(dfsamples2[j,"SEdate"])
     }
   }
   
@@ -67,12 +68,97 @@ for (i in 1:length(rain.site)){
   dfsamples2$Hbpdate <- strptime(dfsamples2$start_date, format="%m/%d/%Y %H:%M")
   dfsamples2$Hepdate <- strptime(dfsamples2$end_date, format="%m/%d/%Y %H:%M")
 
+  #Run scripts: 1. Hydrovol, 2. RMeventsSamples, 3. WQcompos
   dfsamples3 <- Hydrovol(dfQ=dfQ,Q="VALUE",time="pdate",df.dates=dfsamples2,bdate="Hbpdate",edate="Hepdate")
+  dfsamples4 <- RMeventsSamples(df=dfRain,ieHr=12,rain="rain",time="pdate",dfsamples=dfsamples3,bdate="Hbpdate",edate="Hepdate")
+  dfsamples5 <- WQcompos(df.samples=dfsamples4,sampleID="USGS_ID",parms="Human_virus",volume="event.vol")
+  cf2liters <- 28.316846592
+  dfsamples5$Load_Human_virus <- dfsamples5$Human_virus*dfsamples5$event.vol*cf2liters
+  dfsamples5$Daily_Human_virus <- dfsamples5$Load_Human_virus/dfsamples5$duration*24
+    
+  # write file with date in name
+  filenm <- paste(Rlocal,"/MMSD_virus/Virus2/",rain.site[i],"/",rain.site[i],"_",format(Sys.time(), "%b%d%Y_%H%M"),".txt",sep="")
+  write.table(dfsamples5,filenm,sep="\t",row.names=F)
   
-  #events <- RMevents(dfRain,ieHr=6,rainthresh=5.1,rain="rain",time="pdate")[1]
-
 }
 
+## End of data compilation
+################################################################################################################################
+
+# Graph events vs baseflow for individual sites
+
+df <- subset(dfsamples5,!is.na(Human_virus))
+
+bwplot((df$Human_virus+0.01)~df$Site.Name|df$Sample.Type.x,data=df,
+       xlab="",ylab="Pathogen concentrations (gc/L)",
+       main="Event type: Sum of human viruses",
+       scales = list(y = list(log=10)))
+
+bwplot((df$Load_Human_virus+0.01)~df$Site.Name|df$Sample.Type.x,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Sum of human virus load",
+       scales = list(y = list(log=10)))
+
+bwplot((df$Load_Human_virus+0.01)~df$Site.Name|df$Event.Type,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Sum of human virus load",
+       scales = list(y = list(log=10)))
+
+bwplot((df$Daily_Human_virus+0.01)~df$Site.Name|df$Event.Type,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Daily mean human virus load",
+       scales = list(y = list(log=10)))
+
+
+######## Graph loadings for conditional graphs based on sites and event types  #########
+
+rain.site <- c("MenoFalls","LMDonges","Underwood","Honey","70th","16th")
+site <- c("Meno Falls","Donges Bay","Underwood","Honey","70th St.","16th St.")
+
+# Combine data from all sites
+for (i in 1:length(site)){
+
+  filenm <- paste(Rlocal,"/MMSD_virus/Virus2/",rain.site[i],"/",rain.site[i],"_Mar05.txt",sep="")
+  dfsite <- read.delim(filenm)
+  if(i==1) df <- dfsite
+  if(i>1) df <- rbind(df,dfsite)
+  
+}
+
+bwplot((df$Daily_Human_virus+0.01)~df$Site.Name|df$Event.Type,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Daily mean human virus load",
+       scales = list(y = list(log=10),x=list(rot=90)))
+
+bwplot((df$Daily_Human_virus+0.01)~df$Event.Type|df$Site.Name,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Daily mean human virus load",
+       scales = list(y = list(log=10),x=list(rot=90)))
+
+bwplot((df$Daily_Human_virus+0.01)~df$Sample.Type.x|df$Site.Name,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Daily mean human virus load",
+       scales = list(y = list(log=10),x=list(rot=90)))
+
+bwplot((df$Load_Human_virus+0.01)~df$Sample.Type.x|df$Site.Name,data=df,
+       xlab="",ylab="Pathogen load (gc)",
+       main="Event type: Daily mean human virus load",
+       scales = list(y = list(log=10),x=list(rot=90)))
+
+plot(df$depth,(df$Human_virus+0.01),log="y")
+
+m1 <- lm((Human_virus+0.01)~depth+Qmax,data=df)
+summary(m1)
+
+subdf <- subset(df,Event.Type != "Baseflow")
+m2 <- lm((Human_virus+0.01)~depth+Qmax,data=subdf)
+summary(m2)
+plot(subdf$depth,(subdf$Human_virus+0.01),log="y")
+
+subdf <- subset(subdf,Human_virus > 0)
+m3 <- lm((Human_virus+0.01)~depth+Qmax,data=subdf)
+summary(m3)
+plot(subdf$depth,(subdf$Human_virus+0.01),log="y")
 
 # Menomonee test data set
 events.MF<- read.delim("D:/srcldata/R/MMSD_virus/Virus2/virus2Prelim5.txt")
