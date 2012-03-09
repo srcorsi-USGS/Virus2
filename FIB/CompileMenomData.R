b13ccb17c392af26032c41b1efa16dbada6d4e7e
Steve Corsi <srcorsi@usgs.gov>
2012-03-09 14:14:25 -0600

    trying to merge FIB with virus data. just need to make sure veriables in the merged df are the same class as the original FIB df." git status

--- FIB/CompileMenomData.R
+++ FIB/CompileMenomData.R
@@ -22,10 +22,10 @@ source(paste(Rlocal,"/Scripts/fxn_drop.unused.factors.R",sep=""))
 df <- read.delim("virus2Prelim5.txt")
 df.orig <- df # archive original copy
 
-FIB <- read.delim(paste(Rlocal,"/MMSD_virus/Virus2/FIB/FIBFeb222012.txt",sep=""),
+FIB <- read.delim(paste(Rlocal,"/MMSD_virus/Virus2/FIB/FIBMar092012.txt",sep=""),
                   as.is = TRUE)
 FIB.orig <- FIB
-#FIB <- FIB[,-which(names(FIB)=="Notes")]
+FIB <- FIB[,-which(names(FIB)=="Notes")]
 
 #Remove QA/QC samples
 FIB <- FIB[-which(FIB$Event=="QA/QC"|is.na(FIB$FT.)),]
@@ -80,18 +80,71 @@ IDnum <- substr(FIB$Site.Code,IDchar-2,IDchar)
 FIB$Study.Sample.ID <- paste(IDabbrev,IDnum)
 length(FIB$Study.Sample.ID)
 
-df.orig <- df
-FIB.orig <- FIB
-df <- df.orig
-FIB <- FIB.orig
-
-test <- merge(df,FIB,by=c("Ddate","Abb2"),all.y=F)
-
-test <- merge(df,FIB,by=c("Ddate","Abb2"),all.y=F)
-
+df.orig2 <- df
+FIB.orig2 <- FIB
+df <- df.orig2
+FIB <- FIB.orig2
 
 
 df$pdate <- strptime(df$Collection.Start.Date,format="%d-%b-%y")
+df$SEpdate <- strptime(df$SEdate, format="%m/%d/%Y %H:%M")
 df$Ddate <- as.Date(df$pdate)
 FIB$pdate <- strptime(FIB$Sample.Date,format="%d-%b-%y")
 FIB$Ddate <- as.Date(FIB$pdate)
+
+
+FIBpdate <- FIB$pdate + 24*3600-1
+for (i in 1:nrow(FIB)) {
+  timediff <- difftime(FIBpdate[i],df$SEpdate,units="days") # difference in times from FIB and virus samples
+
+  nearby <- which(timediff > 0) # which samples were collected before delivery and not more than two days
+  if (min(timediff[nearby]) > 4){
+    final.sample[i] <- NA
+  } else{
+  site.match <- nearby[which(df[nearby,"Abb2"]==FIB$Abb.[i])] # which samples match site from FIB sample
+  final.sample[i] <- site.match[which(timediff[site.match] == min(timediff[site.match]))]
+  }
+}
+
+# Mannually check and fill in NAs
+which(is.na(final.sample)) #5  46  81 121 159
+FIB[which(is.na(final.sample)),"pdate"] #"2009-06-15" "2009-06-15" "2009-06-15" "2009-06-15" "2009-06-15"
+
+final.sample[5] <- 5 # HW 006
+final.sample[46] <- 47 # LD 005
+final.sample[81] <- 99 # MC 007
+final.sample[121] <- 133 # MF 003
+final.sample[159] <- 175 # MW 006
+
+
+df2 <- df
+FIBnames <- names(FIB)
+FIBnames.orig <- FIBnames
+for (i in 1:length(FIBnames)) {
+  
+  if(sum(FIBnames[i]==names(df))>0) FIBnames[i] <- paste(FIBnames[i],"FIB",sep="")
+}
+     
+for (i in 1:length(FIBnames)) df2[,FIBnames[i]] <- NA
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!   NEED TO FIX THIS -- NEED TO SET NEW VARIABLES IN DF2 TO SAME CLASS AS FIB VARIABLES
+for (i in 1:length(FIBnames)) class(df2[,FIBnames[i]]) <- class(FIB[,FIBnames.orig[i]])
+
+for (i in 1:nrow(FIB)) df2[final.sample[i],FIBnames] <- FIB[i,FIBnames.orig]
+
+i <- 0
+
+i <- i + 1
+df2[final.sample[i],FIBnames] <- FIB[i,FIBnames.orig]
+
+
+
+
+value <- numeric(nrow(df))
+value <- NA
+data.frame(value)
+
+df2 <- cbind(df,data.frame(
+test <- merge(df,FIB,by=c("Ddate","Abb2"),all.y=F)
+
