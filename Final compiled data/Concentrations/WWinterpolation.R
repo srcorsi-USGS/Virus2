11cb306554b6b166277433de61818ea69ea40d75
Steve Corsi <srcorsi@usgs.gov>
2012-03-29 15:33:40 -0500

    Added interpolation of wastewater pathogens for concentrations file

--- Final compiled data/Concentrations/WWinterpolation.R
+++ Final compiled data/Concentrations/WWinterpolation.R
@@ -20,33 +20,51 @@ dfPathFIB <- read.delim(paste(conc.dir,"/Path_FIB_WQ_Hy_All.txt",sep=""))
 
 
 # Take mean of all samples collected on the same date: one from JI and one from SS.
-dfWW$Collection.Start.Date <- as.character(dfWW$Collection.Start.Date)
-WWdates <- unique(dfWW$Collection.Start.Date)
+WWdates <- unique(as.character(dfWW$Collection.Start.Date))
 for (i in 1:length(WWdates)){
   subdf <- subset(dfWW,dfWW$Collection.Start.Date==WWdates[i])
   if(i==1) WWmean <- as.data.frame(t(mean(subdf[,5:26])))
   else WWmean <- rbind(WWmean,as.data.frame(t(mean(subdf[,5:26]))))
 }
 WWmean <- cbind(WWdates,WWmean)
-names(WWmean)[1] <- "date"
 
 #Generate POSIXct dates and add 3.5 days for mean of sample period
-WWmean$pdate <- as.POSIXct(strptime(WWmean$date,format= "%d-%b-%y",tz="CST6CDT")) +3.5*24*3600
-#convert to GMT
-WWmean$pdate <- as.POSIXct(strptime(format(WWmean$pdate,tz="GMT",usetz=TRUE),format="%Y-%m-%d %H:%M",tz="GMT"))
-
+WWmean$pdate <- as.POSIXct(strptime(WWmean$WWdates,format= "%d-%b-%y",tz="CST6CDT")) +3.5*24*3600
 dfPathFIB$Ebpdate <- as.POSIXct(strptime(dfPathFIB$Ebpdate,format=  "%m/%d/%Y %H:%M",tz="GMT"))
+WWmean$pdate <- strptime(format(as.POSIXct(WWmean$pdate),tz="GMT",usetz=TRUE),format="%Y-%m-%d %H:%M",tz="GMT")
 nsamples <- length(dfPathFIB$Ebpdate)
-WWcols <- ncol(WWmean)
 for (i in 1:nsamples){
-  dtime <- difftime(dfPathFIB$Ebpdate[i],WWmean$pdate)
-  minWWdate <- which(WWmean$pdate==min(WWmean$pdate))
-  if(dfPathFIB$Ebpdate[i]<min(WWmean[minWWdate,"pdate"]) WWvalues <- WWmean[minWWdate,2:WWcols]
-  else {
-    WWsub <- WWmean[dtime>0,]
-    WWsub[which(WWsub$pdate==max(WWsub$pdate)),"pdate"
 
+  WWcols <- ncol(WWmean)
+  if(dfPathFIB$Ebpdate[i]<min(WWmean$pdate)) {
+    WWinterp1 <- WWmean[which(dtime==max(dtime)),2:(WWcols-1)]
+  } else{
+    #Determine closest date of WW sample previous to stream sample
+    subWW <- subset(WWmean,pdate<dfPathFIB$Ebpdate[i])
+    WWmean1 <- subWW[which(subWW$pdate==max(subWW$pdate)),2:WWcols]
+
+    #Determine closest date of WW sample after to stream sample
+    subWW <- subset(WWmean,pdate>=dfPathFIB$Ebpdate[i])
+    WWmean2 <- subWW[which(subWW$pdate==min(subWW$pdate)),2:WWcols]
+    Wcols2 <- ncol(WWmean2)
+    
+    #Interpolate concentrations of WW to sample date
+    dtimeWW <- as.numeric(difftime(WWmean2$pdate,WWmean1$pdate,units="days"))
+    dtimeS1 <- as.numeric(difftime(dfPathFIB$Ebpdate[i],WWmean1$pdate,units="days"))
+    timefraction <- dtimeS1/dtimeWW
+    WWinterp1 <- WWmean1[1:(Wcols2-1)] + (WWmean2[1:(Wcols2-1)]-WWmean1[1:(Wcols2-1)])*timefraction
+  }
   
+  #Add interpolated values to data fram
+  if(i == 1) WWinterp <- WWinterp1
+  else WWinterp <- rbind(WWinterp,WWinterp1)
+}
 
+names(WWinterp) <- paste("WW.",names(WWinterp),sep="")
+dfPathFIBWW <- cbind(dfPathFIB,WWinterp)
 
+write.table(dfPathFIBWW,paste(conc.dir,"/PathWWFIB.txt",sep=""),sep="\t",row.names=F)
+
+df<-dfPathFIBWW
+plot(log10(10+df$WW.Enterovirus),log10(0.01+df$Enterovirus))
 
